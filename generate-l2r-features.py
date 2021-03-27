06e402d603f14b49d72d5f9cbd1fb90508fe1804
Niels Bauman <nielsbauman@gmail.com>
2021-03-27 17:02:57 +0100

    Extract l2r feature calculation function

--- generate-l2r-features.py
+++ generate-l2r-features.py
@@ -1,98 +1,47 @@
 import math
 import random
 from pyserini.index import IndexReader
+from l2r_utils import compute_features, read_topics
 
-TOTAL_DOCS = 8841823
-LAMBDA = 0.5 # LMIR.JM hyperparameter
-MU = 2000 # LMIR.DIR hyperparameter
-
-def read_topics(file_name):
-	file = open(file_name)
-
-	topics = {}
-	for line in file:
-		line = line.strip().split("\t")
-		# Try and parse the keys into integers
-		try:
-			topic_key = int(line[0])
-		except ValueError:
-			topic_key = line[0]
-		topics[topic_key] = {
-			'title': line[1],
-		}
-
-	return topics
-
-def compute_features(index_reader, query, qid, docid, target):
-	# BM25 score
-	bm25_score = index_reader.compute_query_document_score(docid, query)
-
-	# TF info
-	tf = index_reader.get_document_vector(docid)
-	total_terms = sum(tf.values())
-
-	# Cumulative variables
-	tf_idf_sum = 0
-	jm = 1
-	dirich = 1
-
-
-	for word in tf.keys():
-		if word in query:
-			counts = index_reader.get_term_counts(word, analyzer=None)
-			df = counts[0] # document frequency
-			cf = counts[1] # collection frequency
-
-			# TF-IDF
-			idf = math.log(math.e, (TOTAL_DOCS / max(df, 1)))
-			tf_idf_sum += (tf[word] / total_terms) * idf
-
-			# Jelineck-Mercer (JM) smoothing (LMIR.JM)
-			p_ml = tf[word] / total_terms
-			p_global = tf[word] / max(cf, 1)
-			jm *= (1 - LAMBDA) * p_ml + LAMBDA * p_global
-
-			# Dirichlet smoothing (LMIR.DIR), the probabilites exceed 1 here for some reason
-			dirich *= (tf[word] + MU * cf) / (total_terms + MU)
-
-	bm25_score = "{:.8f}".format(bm25_score)
-	tf_idf_sum = "{:.8f}".format(tf_idf_sum)
-	jm = "{:.8f}".format(jm)
-	dirich = "{:.8f}".format(dirich)
-	line_format = f"{target} qid:{qid} 1:{bm25_score} 2:{tf_idf_sum} 3:{total_terms} 4:{jm} 5:{dirich} #{docid}\n"
-	# print(line_format)
-	return line_format
 
 def get_negative_docid(document_count, docid):
-	negative_docid = random.randint(1, document_count)
-	while negative_docid == int(docid):
-		negative_docid = random.randint(1, document_count)
-	return negative_docid
+    negative_docid = random.randint(1, document_count)
+    while negative_docid == int(docid):
+        negative_docid = random.randint(1, document_count)
+    return negative_docid
 
-def main(queries_file, qrels_file, output_file, separator, write_negative):
-	queries = read_topics(queries_file)
-	index_reader = IndexReader('indexes/msmarco-passage')
-	document_count = int(index_reader.stats()['documents'])
-	qrels = open(qrels_file, 'r')
 
-	with open(output_file, 'w') as output_file_handle:
-		for line in qrels:
-			line = line.strip().split(separator)
+def format_qrel_line(target, qid, features, docid):
+    return f"{target} qid:{qid} 1:{features.bm25_score} 2:{features.tf_idf_sum} 3:{features.total_terms} 4:{features.jm} 5:{features.dirich} #{docid}\n"
 
-			qid = int(line[0])
-			docid = line[2]
-			target = line[3]
-			query = queries[qid]['title']
 
-			output_file_handle.write(compute_features(index_reader, query, qid, docid, target))
+def main(queries_file, qrels_file, output_file, write_negative):
+    queries = read_topics(queries_file)
+    index_reader = IndexReader('indexes/msmarco-passage')
+    document_count = int(index_reader.stats()['documents'])
+    qrels = open(qrels_file, 'r')
 
-			# The evaluation set doesn't need negative examples.
-			if write_negative:
-				negative_docid = str(get_negative_docid(document_count, docid))
-				output_file_handle.write(compute_features(index_reader, query, qid, negative_docid, 0))
+    with open(output_file, 'w') as output_file_handle:
+        for line in qrels:
+            line = line.strip().split('\t')
+
+            qid = int(line[0])
+            docid = line[2]
+            target = line[3]
+            query = queries[qid]['title']
+
+            features = compute_features(index_reader, query, docid)
+            output_file_handle.write(format_qrel_line(target, qid, features, docid))
+
+            # The evaluation set doesn't need negative examples.
+            if write_negative:
+                negative_docid = str(get_negative_docid(document_count, docid))
+                features = compute_features(index_reader, query, negative_docid)
+                output_file_handle.write(format_qrel_line(0, qid, features, negative_docid))
 
 
 if __name__ == '__main__':
-	# Uncommented by default to avoid running a long command (~20 minutes or so)
-	# main('queries.train.tsv', 'qrels.train.tsv', 'ranklib-features/data_ranklib-train.txt', '\t', True)
-	main('msmarco-test2019-queries.tsv', 'runs/run.msmarco-test2019-queries-bm25.trec', 'ranklib-features/data_ranklib-test.txt', ' ', False)
+    # Uncommented by default to avoid running a long command (~20 minutes or so)
+    # main('queries.train.tsv', 'qrels.train.tsv', 'ranklib-features/data_ranklib-train.txt', True)
+    main('msmarco-test2019-queries.tsv', 'runs/run.msmarco-test2019-queries-bm25.trec',
+         'ranklib-features/data_ranklib-test.txt', False)
---
+++ l2r_utils.py
@@ -0,0 +1,62 @@
+import math
+
+TOTAL_DOCS = 8841823
+LAMBDA = 0.5  # LMIR.JM hyperparameter
+MU = 2000  # LMIR.DIR hyperparameter
+
+
+def read_topics(file_name):
+    file = open(file_name)
+
+    topics = {}
+    for line in file:
+        line = line.strip().split("\t")
+        # Try and parse the keys into integers
+        try:
+            topic_key = int(line[0])
+        except ValueError:
+            topic_key = line[0]
+        topics[topic_key] = {
+            'title': line[1],
+        }
+
+    return topics
+
+
+def compute_features(index_reader, query, docid):
+    # BM25 score
+    bm25_score = index_reader.compute_query_document_score(docid, query)
+
+    # TF info
+    tf = index_reader.get_document_vector(docid)
+    total_terms = sum(tf.values())
+
+    # Cumulative variables
+    tf_idf_sum = 0
+    jm = 1
+    dirich = 1
+
+    for word in tf.keys():
+        if word in query:
+            counts = index_reader.get_term_counts(word, analyzer=None)
+            df = counts[0]  # document frequency
+            cf = counts[1]  # collection frequency
+
+            # TF-IDF
+            idf = math.log(math.e, (TOTAL_DOCS / max(df, 1)))
+            tf_idf_sum += (tf[word] / total_terms) * idf
+
+            # Jelineck-Mercer (JM) smoothing (LMIR.JM)
+            p_ml = tf[word] / total_terms
+            p_global = tf[word] / max(cf, 1)
+            jm *= (1 - LAMBDA) * p_ml + LAMBDA * p_global
+
+            # Dirichlet smoothing (LMIR.DIR), the probabilites exceed 1 here for some reason
+            dirich *= (tf[word] + MU * cf) / (total_terms + MU)
+
+    return {
+        'bm25_score': "{:.8f}".format(bm25_score),
+        'tf_idf_sum': "{:.8f}".format(tf_idf_sum),
+        'jm': "{:.8f}".format(jm),
+        'dirich': "{:.8f}".format(dirich),
+    }
